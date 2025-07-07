cb3fdf41beb31406260542a8a90b4abe81122146
Jat21in <mittaljatin2004@gmail.com>
2025-07-07 19:29:06 +0530

    doneee

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: 'PORTFLIX',
+  description: 'PORTFLIX',
+  generator: 'Jatin',
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+        {/* For PNG favicon: */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
